00982b5ed1264109bdc8165b46dc6ce1d6b527cb
Christophe Künzi <christophe.kuenzi@gmail.com>
2026-01-31 13:36:54 +0100

    last files

--- scenes/buttonGlobe.gd
+++ scenes/buttonGlobe.gd
@@ -1,14 +1,21 @@
 extends TextureButton
 
 @onready var panel = $"../Panel"  # Ajuste le chemin selon ta structure
+var zoomLayer
+var cabinetArlequina
 
 func _ready():
 	# Cache le panel au démarrage
 	panel.visible = false
+
+	var cabinetArlequina = get_tree().root.get_node("Cabinet-arlequina")
 	
 	# Connecte le signal de clic
 	pressed.connect(_on_button_pressed)
 
 func _on_button_pressed():
 	# Affiche le panel quand on clique
+	zoomLayer = cabinetArlequina.get_node("globeZoomLayer")
 	panel.visible = !panel.visible
+	
+	
