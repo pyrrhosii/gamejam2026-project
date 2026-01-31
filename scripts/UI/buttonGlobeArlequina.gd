b553500f8da207bfc7febcaaf066cd94980c0b52
Catou <catou.martini@gmail.com>
2026-01-31 22:12:06 +0100

    Scène sur scène et nettoyage de code

--- scripts/UI/buttonGlobeArlequina.gd
+++ scripts/UI/buttonGlobeArlequina.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-@onready var panel = $"../Panel"  # Ajuste le chemin selon ta structure
+@onready var panel = $dialogueBoxTableau/Panel
 var zoomLayer
 var cabinetArlequina
 var downText
@@ -9,15 +9,13 @@ func _ready():
 	# Cache le panel au démarrage
 	panel.visible = false
 
-	cabinetArlequina = get_tree().root.get_node("Cabinet-arlequina")
-	downText = cabinetArlequina.get_node("dialogueBoxTableau/Panel/container/downText")
+	downText = $dialogueBoxTableau/Panel/container/downText
 	
 	# Connecte le signal de clic
 	pressed.connect(_on_button_pressed)
 
 func _on_button_pressed():
 	# Affiche le panel quand on clique
-	zoomLayer = cabinetArlequina.get_node("globeZoomLayer")
 	downText.text = Dialogues.INFORMATIONS["GLOBE"]["ARLEQUINA"]["firstClick"]
 	panel.visible = !panel.visible
 	
