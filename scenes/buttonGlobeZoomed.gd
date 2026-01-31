69ccc1c0d92e135a7e8066b6975ab4f176d4b25e
Catou <catou.martini@gmail.com>
2026-01-31 15:30:15 +0100

    Script pour bouton globe

---
+++ scenes/buttonGlobeZoomed.gd
@@ -0,0 +1,30 @@
+extends TextureButton
+
+@onready var panel = $"../Panel"  # Ajuste le chemin selon ta structure
+#@onready var textureButton = $TextureButton
+
+var zoomLayer
+var zoom
+
+func _ready():
+	# Cache le panel au démarrage
+	panel.visible = false
+
+	zoom = get_tree().root.get_node("ZonesCliquables")
+	
+	# Connecte le signal de clic
+	self.mouse_entered.connect(_on_button_hover)
+	self.mouse_exited.connect(_on_button_exit)
+	self.pressed.connect(_on_button_click)
+
+func _on_button_hover():
+	# Affiche le panel quand on clique
+	panel.visible = !panel.visible
+	
+func _on_button_exit():
+	# Affiche le panel quand on clique
+	panel.visible = !panel.visible
+	
+func _on_button_click():
+	panel.visible = !panel.visible
+	
