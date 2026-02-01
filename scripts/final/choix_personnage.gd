fa13db8329cef6e96d23c7fc2482623685539e98
Christophe Künzi <christophe.kuenzi@gmail.com>
2026-02-01 14:16:08 +0100

    adding new stuff

--- scripts/final/choix_personnage.gd
+++ scripts/final/choix_personnage.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,4 +13,9 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
-		get_tree().change_scene_to_file("res://scenes/final/introduction_capitan.tscn")
+		get_tree().change_scene_to_file("res://scenes/final/cabinet_arlequina_final.tscn")
+
+
+func _on_area_2d_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
+		get_tree().change_scene_to_file("res://scenes/cabinet_pantalone.tscn")
