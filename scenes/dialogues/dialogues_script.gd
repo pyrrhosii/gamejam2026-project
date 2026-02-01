0326bc2c7a8f1c26f479c357f5c7484e2d12a5d8
Catou <catou.martini@gmail.com>
2026-02-01 14:59:25 +0100

    Ajouts scène Arlequina

--- scenes/dialogues/dialogues_script.gd
+++ scenes/dialogues/dialogues_script.gd
@@ -12,5 +12,7 @@ func _on_button_pressed() -> void:
 	textIndex = (textIndex + 1) 
 	if textIndex == parts.size():
 		hide()
+		textIndex = 0
+		DialogConst.dialogIsOpen = false
 	else:
 		label.text = parts[textIndex]
--- scripts/final/cabinet_arlequina_final.gd
+++ scripts/final/cabinet_arlequina_final.gd
@@ -1,20 +1,30 @@
 extends Node2D
 
-@onready var panel = $dialogueBoxTableau/Panel
-@onready var downText = $dialogueBoxTableau/Panel/container/downText
 @onready var map_scene = preload("res://scenes/final/zoomed_globe_arlequina_final.tscn")
-func _ready():
-	# Cache le panel au démarrage
-	panel.visible = false
+@onready var dialogue_capitan = $DialogueCapitan
+@onready var dialogue_brighella = $DialogueBrighella
 
-func ouvrir_information():
-	# Affiche le panel quand on clique
-	downText.text = Dialogues.INFORMATIONS["GLOBE"]["ARLEQUINA"]["firstClick"]
-	panel.visible = !panel.visible
+func _ready() -> void:
+	dialogue_capitan.visible = false
+	dialogue_brighella.visible = false
+
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
 		print("On a cliqué sur on ouvre le globe!")
-		ouvrir_information()
 		var map = map_scene.instantiate()
 		add_child(map)
+
+
+func _on_capitan_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
+		if DialogConst.dialogIsOpen == false:	
+			dialogue_capitan.visible = true
+			DialogConst.dialogIsOpen = true
+			
+
+func _on_brighella_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
+		if DialogConst.dialogIsOpen == false:	
+			dialogue_brighella.visible = true
+			DialogConst.dialogIsOpen = true
