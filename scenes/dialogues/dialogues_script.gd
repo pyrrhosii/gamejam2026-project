e2c6c5ecc2f31694de02c8810aa9069aff504353
Catou <catou.martini@gmail.com>
2026-02-01 12:13:46 +0100

    Dialogues Dynamiques

---
+++ scenes/dialogues/dialogues_script.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var parts: Array[String] = ["Texte1","Texte2"]
+
+@onready var label = $PanelContainer/RichTextLabel
+var textIndex = 0
+
+func _ready() -> void:
+	label.text = parts[textIndex]
+
+func _on_button_pressed() -> void:
+	textIndex = (textIndex + 1) 
+	if textIndex == parts.size():
+		hide()
+	else:
+		label.text = parts[textIndex]
--- scripts/archives/cabinet_arlequina_test_cm.gd
+++ scripts/archives/cabinet_arlequina_test_cm.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
-@onready var panel = $dialogueBoxTableau/Panel
-@onready var downText = $dialogueBoxTableau/Panel/container/downText
-@onready var map_scene = preload("res://scenes/final/zoomed_globe_arlequina_final.tscn")
+@onready var dialogue_rouge = $DialogueRouge
+@onready var dialogue_vert = $DialogueVert
+@onready var map_scene = preload("res://scenes/zones_cliquables.tscn")
 func _ready():
 	# Cache le panel au démarrage
-	panel.visible = false
+	dialogue_rouge.visible = false
+	dialogue_vert.visible = false
 
 func ouvrir_information():
 	# Affiche le panel quand on clique
-	downText.text = Dialogues.INFORMATIONS["GLOBE"]["ARLEQUINA"]["firstClick"]
-	panel.visible = !panel.visible
+	#downText.text = Dialogues.INFORMATIONS["GLOBE"]["ARLEQUINA"]["firstClick"]
+	pass
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
@@ -18,3 +19,9 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		ouvrir_information()
 		var map = map_scene.instantiate()
 		add_child(map)
+
+func _on_button_rouge_pressed() -> void:
+	dialogue_rouge.visible = true
+
+func _on_button_vert_pressed() -> void:
+	dialogue_vert.visible = true
