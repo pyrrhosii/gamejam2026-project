c88f1bddfce609979218d05531b2764220f03201
sdroz <stephane.droz@gmail.com>
2026-02-12 21:56:41 +0100

    fix Dialog bug: il n'y a plus besoin de cliquer 2 fois au début

--- scenes/dialogues/dialogues_script.gd
+++ scenes/dialogues/dialogues_script.gd
@@ -1,25 +1,24 @@
 extends Control
 
-@export var parts: Array[String] = ["Texte1","Texte2"]
-@onready var label = $PanelContainer/RichTextLabel
-var textIndex = 0
+@export var parts: Array[String] = ["Texte1","Texte2", "Texte3"]
+
+@onready var label := $PanelContainer/RichTextLabel
+
+var textIndex := 0
 
 func _ready() -> void:
 	label.text = parts[textIndex]
 	visibility_changed.connect(_on_visibility_changed)
 
 func _on_button_pressed() -> void:
-	print("textIndex", textIndex)
+	textIndex = textIndex + 1
 	if textIndex == parts.size():
 		hide()
-		textIndex = 0
 		DialogConst.dialogIsOpen = false
 	else:
 		label.text = parts[textIndex]
-		textIndex = (textIndex + 1) 
 		
-func _on_visibility_changed():
+func _on_visibility_changed() -> void:
 	if visible:
 		textIndex = 0
 		label.text = parts[textIndex]
-		print ("C'est quoi ce bordel?!")
