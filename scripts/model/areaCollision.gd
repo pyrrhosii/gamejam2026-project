5551b709ee1e1f7d3edfc580ccf253799b7fbe32
sdroz <stephane.droz@gmail.com>
2026-02-12 22:37:27 +0100

    simplify verify sequence

--- scripts/model/areaCollision.gd
+++ scripts/model/areaCollision.gd
@@ -1,70 +1,50 @@
 extends Node2D
 
-var sequence_correcte = ["Sultanat", "Chinapon", "Ottoman","Capistan","Dindia","Europea"]
-var sequence_joueur = []  # Ce que le joueur clique
-var etape_actuelle = 0    # Combien de clics corrects
+var correctSequence := ["Sultanat", "Chinapon", "Ottoman","Capistan","Dindia","Europea"]
+var currentStep := 0    # Combien de clics corrects
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-	
-func showAndHide(panel: Panel, landName: String) -> void:
-	print("On clique sur une marque", landName)
-	panel.visible = true
-	verifySequence(landName)
+func showAndHide(panel: Panel, country: String) -> void:
+	print("On clique sur une marque", country)
+	panel.show()
+	verifySequence(country)
 	await get_tree().create_timer(1.0).timeout
-	panel.visible = false
+	panel.hide()
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
 		showAndHide($AreaSultanat/Sultanat/Panel, "Sultanat" )
 
-func _on_area_2d_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_2d_2_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
 		showAndHide($AreaChinapon/Chinapon/Panel, "Chinapon" )
 
-func _on_area_2d_3_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_2d_3_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
 		showAndHide($AreaOttoman/Ottoman/Panel, "Ottoman" )
 
-func _on_area_capistan_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_capistan_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
 		showAndHide($AreaCapistan/Capistan/Panel, "Capistan" )
 
-func _on_area_dindia_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_dindia_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
 		showAndHide($AreaDindia/Dindia/Panel, "Dindia" )
 
-func _on_area_europea_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_europea_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MASK_LEFT and event.is_pressed():
 		showAndHide($AreaEuropea/Europea/Panel, "Europea" )
 		
-func verifySequence(buttonName):
-	# Ajoute le clic du joueur
-	sequence_joueur.append(buttonName)
-	
-	# Vérifie si c'est le bon ordre
-	if buttonName == sequence_correcte[etape_actuelle]:
-		etape_actuelle += 1
-		#texte.text = "✅ BON ! Étape " + str(etape_actuelle) + "/3"
-		print("✅ BON ! Étape " + str(etape_actuelle) + "/5")
-		
-		# SÉQUENCE COMPLÈTE !
-		if etape_actuelle == 6:
-			$CanvasLayer/PanelSuccess.visible = true
-			await get_tree().create_timer(6.0).timeout
-			get_tree().change_scene_to_file("res://scenes/final/final_capitan_arlequina.tscn")
-			#texte.visible = true
-			#texte.text = "🎉 PARFAIT ! Tu as réussi !"
-			reset_sequence()
-	else:
+func verifySequence(country) -> void:
+	if country != correctSequence[currentStep]:
 		print("❌ MAUVAIS ORDRE ! Recommence.")
-		#texte.visible = true
-		#texte.text = "❌ MAUVAIS ORDRE ! Recommence."
-		reset_sequence()
-
-func reset_sequence():
-	sequence_joueur.clear()
-	etape_actuelle = 0
-	#texte.text = "Clique : Rouge → Bleu → Vert"
+		currentStep = 0
+		return
+	
+	print("✅ BON ! Étape " + str(currentStep) + "/5")
+	currentStep += 1
+		
+	if currentStep == correctSequence.size():
+		$CanvasLayer/PanelSuccess.show()
+		currentStep = 0
+		await get_tree().create_timer(6.0).timeout
+		get_tree().change_scene_to_file("res://scenes/final/final_capitan_arlequina.tscn")
